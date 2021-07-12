a2021cf0ca8ee264c568ca3563ddd13f41ff002b
poly000 <1348292515@qq.com>
2021-07-13 00:19:48 +0800

    refactor

---
+++ src/data.rs
@@ -0,0 +1,29 @@
+//! This file was licensed under CC-BY-NC-SA 3.0,
+//! due to https://zh.moegirl.org.cn/
+
+/// from https://zh.moegirl.org.cn/%E7%BA%B1%E4%B8%96%E9%87%8C
+pub static sayori_words: [&'static str; 88] = [
+    "adventure", "alone", "amazing", "awesome",
+    "beauty", "bed", "bliss", "broken",
+    "calm", "charm", "cheer", "childhood",
+    "clumsy", "color", "comfort", "cry",
+    "dance", "dark", "daydream", "dazzle",
+    "death", "defeat", "depression", "embrace",
+    "empty", "excitement", "extraordinary", "family",
+    "fear", "feather", "fireflies", "fireworks",
+    "flower", "flying", "forgive", "friends",
+    "fun", "grief", "happiness", "heart",
+    "holiday", "hope", "hopeless", "hurt",
+    "joy", "laugh", "lazy", "loud",
+    "love", "lucky", "marriage", "memories",
+    "misery", "misfortune", "music", "nature",
+    "ocean", "pain", "party", "passion",
+    "peaceful", "play", "prayer", "precious",
+    "promise", "rainbow", "raincloud", "romance",
+    "rose", "sadness", "scars", "shame",
+    "silly", "sing", "smile", "sparkle",
+    "special", "sunny", "sunset", "sweet",
+    "tears", "together", "tragedy", "treasure",
+    "unrequited", "vacation", "warm", "wonderful"
+];
+
--- src/main.rs
+++ src/main.rs
@@ -7,31 +7,10 @@ enum Charactor {
     Natsuki,
 }
 
+mod data;
+
 static sayori_words_set: Lazy<HashSet<&'static str>> = Lazy::new(||{
-    let sayori_words: [&'static str; 88] =[
-        "adventure", "alone", "amazing", "awesome",
-        "beauty", "bed", "bliss", "broken",
-        "calm", "charm", "cheer", "childhood",
-        "clumsy", "color", "comfort", "cry",
-        "dance", "dark", "daydream", "dazzle",
-        "death", "defeat", "depression", "embrace",
-        "empty", "excitement", "extraordinary", "family",
-        "fear", "feather", "fireflies", "fireworks",
-        "flower", "flying", "forgive", "friends",
-        "fun", "grief", "happiness", "heart",
-        "holiday", "hope", "hopeless", "hurt",
-        "joy", "laugh", "lazy", "loud",
-        "love", "lucky", "marriage", "memories",
-        "misery", "misfortune", "music", "nature",
-        "ocean", "pain", "party", "passion",
-        "peaceful", "play", "prayer", "precious",
-        "promise", "rainbow", "raincloud", "romance",
-        "rose", "sadness", "scars", "shame",
-        "silly", "sing", "smile", "sparkle",
-        "special", "sunny", "sunset", "sweet",
-        "tears", "together", "tragedy", "treasure",
-        "unrequited", "vacation", "warm", "wonderful"
-    ];
+    use data::sayori_words;
 
     sayori_words.iter()
                 .map(|refe| *refe)
@@ -40,6 +19,20 @@ static sayori_words_set: Lazy<HashSet<&'static str>> = Lazy::new(||{
 
 
 
+fn filter_words<'a>(words: &[&'a str]) -> Vec<&'a str> {
+    vec!["lol"]
+}
+
 fn main() {
-    
+    let mut charactor;
+
+    println!("Please select the charactor you like: (enter the number near names)");
+    println!();
+    println!("0 Sayori, 1 Yuri, 2 Natsuki");
+    println!();
+
+    charactor = {
+        let num = 
+    }
+
 }
