38d288514888e589beff0e1c0958d61c932b7be3
poly000 <1348292515@qq.com>
2021-07-13 01:23:35 +0800

    update

--- src/data.rs
+++ src/data.rs
@@ -1,8 +1,11 @@
 //! This file was licensed under CC-BY-NC-SA 3.0,
 //! due to https://zh.moegirl.org.cn/
 
+use once_cell::sync::Lazy;
+use std::collections::HashSet;
+
 /// from https://zh.moegirl.org.cn/%E7%BA%B1%E4%B8%96%E9%87%8C
-pub static sayori_words: [&'static str; 88] = [
+static SAYORI_WORDS: [&'static str; 88] = [
     "adventure", "alone", "amazing", "awesome",
     "beauty", "bed", "bliss", "broken",
     "calm", "charm", "cheer", "childhood",
@@ -27,3 +30,57 @@ pub static sayori_words: [&'static str; 88] = [
     "unrequited", "vacation", "warm", "wonderful"
 ];
 
+/// from https://zh.moegirl.org.cn/%E4%BC%98%E9%87%8C
+static YURI_WORDS: [&'static str; 78] = [
+    "afterimage", "agonizing", "ambient", "analysis", "anxiety", "atone",
+    "aura", "breathe", "cage", "captive", "climax", "contamination",
+    "covet", "crimson", "desire", "despise", "destiny", "determination",
+    "disarray", "disaster", "disoriented", "disown", "dream", "effulgent",
+    "electricity", "entropy", "essence", "eternity", "existence", "explode",
+    "extreme", "fester", "fickle", "flee", "frightening", "graveyard",
+    "heavensent", "horror", "imagination", "incapable", "incongruent", "infallible",
+    "inferno", "infinite", "insight", "intellectual", "journey", "judgment",
+    "landscape", "lust", "massacre", "meager", "melancholy", "philosophy",
+    "pleasure", "portrait", "question", "raindrops", "secretive", "sensation",
+    "starscape", "suicide", "tenacious", "time", "uncanny", "uncontrollable",
+    "unending", "universe", "unrestrained", "unstable", "variance", "vertigo",
+    "vibrant", "vitality", "vivacious", "vivid", "whirlwind", "wrath",
+];
+
+/// from https://zh.moegirl.org.cn/%E5%A4%8F%E6%A0%91(%E5%BF%83%E8%B7%B3%E6%96%87%E5%AD%A6%E9%83%A8)
+static NATSUKI_WORDS: [&'static str; 62] = [
+    "anger", "anime", "blanket", "boop",
+    "bouncy", "bubbles", "bunny", "candy",
+    "cheeks", "chocolate", "clouds", "cute",
+    "doki-doki）", "email", "fantasy", "fluffy",
+    "games", "giggle", "hair", "headphones",
+    "heartbeat", "hop", "jump", "jumpy",
+    "kawaii", "kiss", "kitty", "lipstick",
+    "lollipop", "marshmallow", "melody", "milk",
+    "mouse", "nibble", "nightgown", "papa",
+    "parfait", "peace", "pink", "playground",
+    "poof", "pout", "puppy", "pure",
+    "ribbon", "shiny", "shopping", "skipping",
+    "skirt", "socks", "spinning", "sticky",
+    "strawberry", "sugar", "summer", "swimsuit",
+    "twirl", "valentine", "vanilla", "waterfall",
+    "whisper", "whistle"
+];
+
+pub static SAYORI_WORDS_SET: Lazy<HashSet<&'static str>> = Lazy::new(|| {
+    SAYORI_WORDS.iter()
+                .map(|refe| *refe)
+                .collect()
+});
+
+pub static YURI_WORDS_SET: Lazy<HashSet<&'static str>> = Lazy::new(|| {
+    YURI_WORDS.iter()
+                .map(|refe| *refe)
+                .collect()
+});
+
+pub static NATSUKI_WORDS_SET: Lazy<HashSet<&'static str>> = Lazy::new(|| {
+    NATSUKI_WORDS.iter()
+                .map(|refe| *refe)
+                .collect()
+});
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
-use once_cell::sync::Lazy;
-use std::collections::HashSet;
-
 enum Charactor {
     Sayori,
     Yuri,
@@ -8,31 +5,67 @@ enum Charactor {
 }
 
 mod data;
+use data::SAYORI_WORDS_SET;
+use data::YURI_WORDS_SET;
+use data::NATSUKI_WORDS_SET;
 
-static sayori_words_set: Lazy<HashSet<&'static str>> = Lazy::new(||{
-    use data::sayori_words;
+use std::io::Read;
+use std::io::stdin;
 
-    sayori_words.iter()
-                .map(|refe| *refe)
-                .collect()
-});
-
-
-
-fn filter_words<'a>(words: &[&'a str]) -> Vec<&'a str> {
-    vec!["lol"]
-}
-
-fn main() {
-    let mut charactor;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    
 
     println!("Please select the charactor you like: (enter the number near names)");
-    println!();
-    println!("0 Sayori, 1 Yuri, 2 Natsuki");
-    println!();
+    println!("0 Sayori, 1 Yuri, 2 Natsuki\n");
 
-    charactor = {
-        let num = 
+    let charactor = {
+        let mut buf = String::new();
+
+        stdin().read_line(&mut buf)?;
+
+        let num = buf.parse::<u32>()?;
+        
+        match num {
+            0 => Charactor::Sayori,
+            1 => Charactor::Yuri,
+            2 => Charactor::Natsuki,
+            _ => panic!("charactor code was out of bound!"),
+        }
+    };
+
+    println!("Great! Now please input your words until EOF.");
+
+    let mut raw_words = String::new();
+    stdin().read_to_string(&mut raw_words)?;
+
+    let words_list = raw_words.split_whitespace();
+    let result = filter_words(words_list, charactor);
+
+    println!("Result:");
+    for (index, word) in result.into_iter().enumerate() {
+        if index % 2 == 0 {
+            println!("{}", word);
+        } else {
+            print!("{}",word);
+        }
     }
+    println!();
 
+    Ok(())
+}
+
+fn filter_words<'a> (
+    words: impl Iterator<Item=&'a str>,
+    charactor: Charactor)
+-> Vec<&'a str> {
+
+    let words_set = match charactor {
+        Charactor::Sayori => &SAYORI_WORDS_SET,
+        Charactor::Yuri => &YURI_WORDS_SET,
+        Charactor::Natsuki => &NATSUKI_WORDS_SET,
+    };
+
+    words
+    .filter(|s| words_set.contains( s.to_lowercase()))
+    .collect()
 }
